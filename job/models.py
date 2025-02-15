c8bf38b49bdc18234dff20aebeab0b29733ee0e6
belal mahmoud <medo55013@yahoo.com>
2025-02-15 04:06:33 +0300

    startapp

--- job/models.py
+++ job/models.py
@@ -8,10 +8,24 @@ for
 2 html
 3 validation
 '''
+
+'''
+relations indjango
+1-one to many (user-posts) forign key 
+2-many to many(user-groups) same
+3-one to one (user-profile)same
+'''
 JOB_TYPE = (
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
+
+def image_upload(instance,filename):
+    imagename,extension=filename.split(".") #%s/%s mean folder and last %s file if %s.%s maen only one file
+    return "jobs/%s.%s"%(instance.id,extension)
+
+    
+
 class Job(models.Model):#table
     title = models.CharField(max_length=100)#colum
     job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
@@ -20,6 +34,14 @@ class Job(models.Model):#table
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1) 
-  
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)# if category delete all job in this category deleted
+    image = models.ImageField(upload_to=image_upload)
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
--- job/views.py
+++ job/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Job
+def job_list(request):
+    job=Job.objects.all()
+    
+    context ={
+        'job':job
 
-# Create your views here.
+    }
+    return render(request,'job/joblist.html',context)
+
+def job_detail(request,id):
+    jobdetails=Job.objects.get(id=id)
+    context ={
+        'job':jobdetails
+
+    }
+    return render(request,'job/jobdetails.html',context)
